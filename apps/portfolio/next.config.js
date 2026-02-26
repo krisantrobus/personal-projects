3e3c54dcb09ecd9598f06070c1072c64d158d4bb
Kristian Antrobus <kantrobus@gmail.com>
2026-02-26 13:41:25 -0600

    update build

--- apps/portfolio/next.config.js
+++ apps/portfolio/next.config.js
@@ -9,6 +9,8 @@ const { composePlugins, withNx } = require('@nx/next');
 const nextConfig = {
   // Use this to set Nx-specific options
   // See: https://nx.dev/recipes/next/next-config-setup
+  output: 'standalone',
+  distDir: '/dist',
   nx: {},
 };
 
